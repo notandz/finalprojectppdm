7ae5f9bdbaeb56fcb6f4ba54ec32df61419d5564
ditinggalrabi <89618518+ditinggalrabi@users.noreply.github.com>
2023-07-09 23:22:40 +0800

    Dataset Update

--- main_user.py
+++ main_user.py
@@ -32,7 +32,7 @@ def calculate_glcm_features(img):
     return np.concatenate(features)
 
 # Train the model
-def train_model():
+def train_model(k):
     st.write("Training Model...")
     
     # Load image files and labels
@@ -50,7 +50,7 @@ def train_model():
 
     # Train KNN classifier
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
-    model = KNeighborsClassifier(n_neighbors=3)
+    model = KNeighborsClassifier(n_neighbors=k)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     accuracy = metrics.accuracy_score(y_test, y_pred)
@@ -72,54 +72,17 @@ def train_model():
     np.save(dataset_file, dataset)
     st.write("Dataset saved!")
 
-# Correct misclassification
-def correct_misclassification(features, correct_label):
-    # Load the dataset file
-    dataset = np.load(dataset_file)
-    labels = dataset[:, -1]  # Extract the labels
-
-    # Find the index of the misclassified image in the dataset
-    misclassified_index = np.where(np.all(dataset[:, :-1] == features, axis=1))[0][0]
-
-    # Update the label
-    labels[misclassified_index] = correct_label
-
-    # Save the updated dataset
-    np.save(dataset_file, np.column_stack((dataset[:, :-1], labels)))
-
-# Streamlit UI
-# Streamlit UI
-def main():
-    st.set_page_config(layout="wide")
-    st.markdown(
-        """
-        <style>
-        .container {
-            display: flex;
-        }
-        .left_column {
-            width: 50%;
-            padding: 1rem;
-        }
-        .right_column {
-            width: 50%;
-            padding: 1rem;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    
+# Streamlit UI - Home Page
+def home():
     st.title('Happy or Sad Image Classifier')
-
+    
     # Check if model and dataset files exist
     model_exists = os.path.exists(model_file)
     dataset_exists = os.path.exists(dataset_file)
-
-    # Add "Retrain" button
-    if st.button("Retrain"):
-        train_model()
-        model_exists = True
+    
+    # Add "Train Model" button
+    if st.button("Train Model"):
+        st.session_state.page = 'Train'
 
     # Image classification
     file = st.file_uploader("Upload an image")
@@ -156,13 +119,32 @@ def main():
                 st.write(f"Recall: {recall:.2f}")
                 st.write(f"F1-Score: {f1_score:.2f}")
 
-            # Classification result and correction
-            if st.button("Correct"):
-                if prediction == 0:
-                    correct_misclassification(features, 1)
-                else:
-                    correct_misclassification(features, 0)
-                st.write("Misclassification corrected!")
+# Streamlit UI - Train Page
+def train():
+    st.title('Train Model')
+    
+    # Add "Back" button
+    if st.button("Back"):
+        st.session_state.page = 'Home'
+    
+    # Select K value
+    k = st.selectbox("Select K value", [1, 3, 5, 7, 9])
+    
+    # Train the model
+    if st.button("Train", key="train_button"):
+        train_model(k)
+
+# Streamlit App
+def main():
+    # Initialize Streamlit session state
+    if 'page' not in st.session_state:
+        st.session_state.page = 'Home'
+    
+    # Page navigation
+    if st.session_state.page == 'Home':
+        home()
+    elif st.session_state.page == 'Train':
+        train()
 
 if __name__ == "__main__":
     main()
